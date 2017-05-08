3386eb039df1dbc4b142022921ddc674229e4358
maoyanting <fhmaoyanting@163.com>
2017-05-09 00:02:55 +0800

    init

---
+++ helloworld/src/main/java/me/maoyanting/helloworld/App.java
@@ -0,0 +1,13 @@
+package me.maoyanting.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
